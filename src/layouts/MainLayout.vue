142c5722097aee4a9e091f5303cf913d8c3a1f77
TomaszMoroz <tmoroz688@gmail.com>
2025-11-17 15:32:09 +0100

    v++

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -17,7 +17,7 @@
             <q-icon name="shield" class="q-mr-sm" size="sm" color="white" />
           </div>
         </q-toolbar-title>
-        {{ 'v2.3.3 WERSJA TESTOWA' }}
+        {{ 'v2.3.4 WERSJA TESTOWA' }}
       </q-toolbar>
     </q-header>
 
